d92f3332a0e8365705d58b849356342fd2527533
aicma <Alec7@web.de>
2016-05-17 13:14:25 +0200

    mongoose schemas

--- server.js
+++ server.js
@@ -9,6 +9,38 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
+db.once('open', function callback(){
+    var eventSchema = mongoose.Schema({
+        date: { type: Date, default: Date.now },
+        description: String,
+        del_req: Boolean,
+        inv_person: [personSchema],
+        createdBy: userSchema,
+        location: locationSchema
+
+    });
+
+    var userSchema = mongoose.Schema({
+        name: String,
+        firstName: String,
+        isAdmin: Boolean
+    });
+
+    var personSchema = mongoose.Schema({
+        name: String,
+        firstName: String,
+        isMale: Boolean,
+        birthday: Date,
+        involved: [eventSchema]
+    });
+
+    var locationSchema = mongoose.Schema({
+        name: String,
+        employees: [userSchema],
+        events: [eventSchema]
+    })
+})
+
 app.get('/', function(req, res){
     res.send('get request received');
 });
